eb7fd1d29832e78b892f5f7319cb736ee06e2d8c
xingchen.lin <xingchen.lin@ele.me>
2019-11-05 22:35:03 +0800

    dupate

--- main/__init__.py
+++ main/__init__.py
@@ -1,4 +1,5 @@
 from spider import cyber_japan_data
+from model import opensearch
 
 class Main:
 
@@ -6,10 +7,15 @@ class Main:
     return
 
   def Run(self):
-    # 爬取数据并转换
+    obj_opensearch = opensearch.OpenSearch()
     obj_cyber = cyber_japan_data.CyberJapanData()
-    # parsed_cyber_data = obj_cyber.get_data_by_tile(29086, 12887)
-    parsed_cyber_data = obj_cyber.get_data_by_tile(29104, 12914)
-    print(parsed_cyber_data)
+
+    # 切东京范围
+    for x in range(29086, 29120 + 1):
+      for y in range(12887, 12922 + 1):
+        # 爬取数据并转换
+        parsed_cyber_data = obj_cyber.get_data_by_tile(x, y)
+        # 入库
+        obj_opensearch.insert(parsed_cyber_data)
 
     return
---
+++ model/opensearch.py
@@ -0,0 +1,16 @@
+import conf
+
+class OpenSearch:
+
+  obj_conf = conf.Conf()
+
+  def __init__(self):
+    return
+
+  def insert(self, parsed_cyber_data):
+    self.parse(parsed_cyber_data)
+    return
+
+  def parse(self, parsed_cyber_data):
+
+    return
