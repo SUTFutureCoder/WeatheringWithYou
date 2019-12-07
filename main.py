07924c0f8a976e2eaad4da0219b6784a8dda8fc4
xingchen.lin <xingchen.lin@ele.me>
2019-12-07 23:34:12 +0800

    update

--- main.py
+++ main.py
@@ -2,7 +2,17 @@
 #-*-coding:utf-8-*-
 
 import main
+from util.tile_latlon import TiteLation
 
 objRun = main.Main()
-# objRun.Run()
+objRun.Run()
 objRun.RunDrain()
+
+# 分辨率精度证明
+print(TiteLation.tile2latlon(29106, 12905, 15))
+print(TiteLation.tile2latlon(29107, 12905, 15))
+print(TiteLation.tile2latlon(29106, 12906, 15))
+
+# 0.9936 km
+# 分辨率 0.00388125 km = 3.88125m
+
